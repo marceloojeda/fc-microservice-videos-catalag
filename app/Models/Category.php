7e8d7dcbd840dabc6504954d03f58069e5f1b0e5
Marcelo Ojeda <marcelo@upgestao.com.br>
2020-10-06 22:19:19 -0300

    feature/unit-tests Criacao de testes automatizados.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,5 +13,6 @@ class Category extends Model
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = [ 'id' => 'string' ];
+    public $incrementing = false;
 
 }
